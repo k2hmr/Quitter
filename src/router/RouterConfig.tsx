d83f42864a8d2dd264dbe893116f08303220d6c8
k2hmr <k2hmr94@gmail.com>
2023-06-05 23:05:48 +0900

    feat: トップページの基本的なレイアウトを作成

--- src/router/RouterConfig.tsx
+++ src/router/RouterConfig.tsx
@@ -1,3 +1,4 @@
+import TopBar from '@/components/layout/templates/TopBar'
 import { Route, Routes } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -5,7 +6,7 @@ export const RouterConfig = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>テスト</>}>
+        <Route path="/" element={<TopBar />}>
           <Route index element={<>テスト</>} />
         </Route>
       </Routes>
