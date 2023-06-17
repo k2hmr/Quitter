c126262b995924ec6a4d190808b40ee84f64a18f
k2hmr <k2hmr94@gmail.com>
2023-06-17 12:15:16 +0900

    feat: 登録済みならログイン画面に促す

--- src/components/pages/SignupPage.tsx
+++ src/components/pages/SignupPage.tsx
@@ -1,9 +1,16 @@
 import { useFirebaseAuth } from '@/hooks/useFirebaseAuth'
+import { Flex } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import AuthForm from '../layout/molecules/AuthForm'
 
 const SignupPage: React.FC<{}> = () => {
   const { signUp } = useFirebaseAuth()
-  return <AuthForm title="ユーザー登録" isNewUser={true} authFunc={signUp} />
+  return (
+    <Flex justify={'center'} align={'center'} flexDirection={'column'}>
+      <AuthForm title="ユーザー登録" isNewUser={true} authFunc={signUp} />
+      <Link to={'/login'}>登録済みの方はこちら</Link>
+    </Flex>
+  )
 }
 
 export default SignupPage
