b467eee8caa0dfc2caaad378f23dc95dc47f1150
k2hmr <k2hmr94@gmail.com>
2023-07-07 00:07:02 +0900

    feat: マイページで名前、メールアドレス、作成日を表示する

--- src/components/pages/MyPage.tsx
+++ src/components/pages/MyPage.tsx
@@ -2,13 +2,22 @@ import { useContext } from 'react'
 import { Div } from '../layout/atom/Div'
 import { Header } from '../layout/atom/Header'
 import { UserContext } from '@/store/userContext'
+import { AppFlex } from '../layout/atom/Flex'
 
 export const MyPage = () => {
   const { user } = useContext(UserContext)
   return (
     <Div>
       <Header>マイページ</Header>
-      {user ? <Div>名前：{user.name}</Div> : <Div>ログインしてください</Div>}
+      {user ? (
+        <AppFlex flexDirection={'column'}>
+          <Div>名前：{user.name}</Div>
+          <Div>メールアドレス：{user.email}</Div>
+          <Div>作成日：{new Date(user.createdAt).toLocaleString()}</Div>
+        </AppFlex>
+      ) : (
+        <Div>ログインしてください</Div>
+      )}
     </Div>
   )
 }
