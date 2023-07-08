08dcc54deed51b2382bfdf5f6a063583148c61e5
k2hmr <k2hmr94@gmail.com>
2023-07-08 13:56:51 +0900

    feat: 投稿テンプレート一覧ページを追加

--- src/components/layout/atom/Div.tsx
+++ src/components/layout/atom/Div.tsx
@@ -1,8 +1,24 @@
-import { Box } from '@chakra-ui/react'
+import { Box, ResponsiveValue } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 type bProps = {
+  bg?: string
+  p?: ResponsiveValue<
+    | number
+    | (string & {})
+    | 'px'
+    | '-moz-initial'
+    | 'inherit'
+    | 'initial'
+    | 'revert'
+    | 'revert-layer'
+    | 'unset'
+  >
   children: ReactNode
 }
 
-export const Div: React.FC<bProps> = ({ children }) => <Box>{children}</Box>
+export const Div: React.FC<bProps> = ({ bg, p, children, ...rest }) => (
+  <Box bg={bg} p={p} {...rest}>
+    {children}
+  </Box>
+)
--- src/components/pages/MyPage.tsx
+++ src/components/pages/MyPage.tsx
@@ -1,11 +1,12 @@
+import { UserContext } from '@/store/userContext'
 import { useContext } from 'react'
 import { Div } from '../layout/atom/Div'
-import { Header } from '../layout/atom/Header'
-import { UserContext } from '@/store/userContext'
 import { AppFlex } from '../layout/atom/Flex'
+import { Header } from '../layout/atom/Header'
 
 export const MyPage = () => {
   const { user } = useContext(UserContext)
+
   return (
     <Div>
       <Header>マイページ</Header>
